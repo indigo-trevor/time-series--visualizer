93ae2a50748dbaa31093e500d9e662207ca74324
Trevor Robinson <trevor.robinson@indigoslate.com>
2018-04-01 14:20:49 -0700

    showing data via graph from backend json file

--- src/App.js
+++ src/App.js
@@ -4,71 +4,71 @@ import {Line} from 'react-chartjs-2';
 
 const API = '/cpu';
 
+const chart1 = {
+  labels: [0],
+  datasets: [{
+    data: [0],
+    backgroundColor: [
+      '#4DB6AC'
+    ]
+  }]
+};
+
+const Button = (props) => (
+  <button id="update-chart" onClick={props.handleOnClick}>Update</button>
+);
+
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.options = {
-      type: 'line',
-      data: {
-        labels: ['1', '2', '3', '4', '5'],
-        datasets: [{
-          label: "My First dataset",
-          backgroundColor: 'rgba(52, 152, 219, 0.75)',
-          data: [
-            5, 10, 15, 30, 50
-          ]
-        }]
-      },
-      options: {
-        title: {
-          display: false
-        },
-        legend: {
-          display: false
-        },
-        scales: {
-          xAxes: [{
-            display: true
-          }],
-          yAxes: [{
-            type: "linear",
-            display: true,
-            position: "left"
-          }]
-        },
-        responsive: true
-      }
-    }
     this.state = {
-      cpu: []
-    };
+      chartData: chart1,
+      updated: false,
+      cpu: [],
+      cpuKey: []
+    }
+    this.handleUpdate = this.handleUpdate.bind(this);
   }
 
   componentDidMount() {
-    this.canvas = document.querySelector('canvas')
-    this.ctx = this.canvas.getContext('2d')
-    this.chart = new Chart(this.ctx, this.options)
     axios.get(API)
       .then(res => {
-        const cpu = res.data;
+        const cpuResponse = res.data;
+        const cpu = [];
+        const cpuKey = [];
+        for (var i = 0; i < cpuResponse.length; i++) {
+          cpu.push(cpuResponse[i].percent);
+          cpuKey.push(cpuResponse[i].id);
+        }
         this.setState({ cpu });
-        console.log(cpu)
+        this.setState({ cpuKey });
+        this.handleUpdate();
       });
   }
 
+  // Update CPU Chart Values
+  handleUpdate() {
+    var updatedChartData  = {};
+    updatedChartData = chart1;
+    updatedChartData.datasets[0].data = this.state.cpu
+    updatedChartData.labels = this.state.cpuKey
+    const chartData = updatedChartData;
+    console.log(chartData)
+
+// Batching both updates to state in the same call to this.setState
+
+    this.setState({chartData, updated: !this.state.updated});
+
+// for updated, read from what is currently set as updated in state and do the opposite - creating a toggle
+  }
+
   render() {
-    return (
+    return(
       <div>
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <h1>Time-series Visualization</h1>
-              <canvas />
-            </div>
-          </div>
-        </div>
+        <Line data={this.state.chartData} />
+        <Button handleOnClick={this.handleUpdate} />
       </div>
-    )
+    );
   }
 
 }
