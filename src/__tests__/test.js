034311f3ef5444819cd3fa62706a27122b601493
Trevor Robinson <trevor.robinson@indigoslate.com>
2018-04-04 17:53:31 -0700

    added jest and enzyme for testing

---
+++ src/__tests__/test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+
+import App from '../App';
+
+describe('Test plan', () => {
+  it('This is where I would implement my test plan', () => {
+  
+  });
+});
